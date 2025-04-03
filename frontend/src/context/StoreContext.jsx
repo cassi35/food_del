cdc9018a64b66a0001b7162f7555522a50195aba
cassi35 <cgrsobral@gmail.com>
2025-04-03 14:29:15 -0300

    no fronend fiz com que desse um fetch com axios  do meu server sobre o carrinho de compras

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,15 +6,21 @@ const StoreContextProvider = (props)=>{
     const url = 'http://localhost:4000'
     const [token,setToken] = useState("")
     const [food_list,seFoodList] = useState([])
-    const addToCard = (itemId)=>{
+    const addToCard = async (itemId)=>{
         if(!cardItem[itemId]){
             setcardItems((prev)=>({...prev,[itemId]:1}))
         }else{
             setcardItems((prev)=>({...prev,[itemId]:prev[itemId]+1 }))
         }
+        if(token){
+            await axios.post(url+'/api/cart/add',{itemId},{headers:{token}})
+        }
     }   
-    const removeFromCard = (itemId)=>{
+    const removeFromCard = async (itemId)=>{
         setcardItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+        if(token){
+            await axios.post(url+'/api/cart/remove',{itemId},{headers:{token}})
+        }
     }
     const getTotalCartAmount = ()=>{
         let toalAmount = 0
@@ -31,11 +37,16 @@ const StoreContextProvider = (props)=>{
         const response = await axios.get(`${url}/api/food/list`)
         seFoodList(response.data.data)
     }
+    const loadCartData = async (token)=>{
+        const response = await axios.post(url+'/api/cart/get',{},{headers:{token}})
+        setcardItems(response.data.cartData)
+    }
     useEffect(()=>{
         async function loadData(){
             await fetchFood()
             if(localStorage.getItem("token")){
                 setToken(localStorage.getItem("token"))
+                await loadCartData(localStorage.getItem("token"))
             }
         }
         loadData()
