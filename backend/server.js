c5d70811cf423eed82e65863a5974f25e3d76cc8
cassi35 <cgrsobral@gmail.com>
2025-04-03 15:02:56 -0300

    fiz rota de pamentto com stripe

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import path from 'path'
 import userRouter from './routes/userRoute.js'
 import 'dotenv/config.js'
 import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
 //app config 
 const app = express()
 const port = 4000
@@ -22,6 +23,7 @@ app.use('/api/food', foodRouter);
 app.use('/images', express.static(path.join(process.cwd(), 'uploads')));
 app.use("/api/user",userRouter)
 app.use('/api/cart',cartRouter)
+app.use('/api/order',orderRouter)
 app.get('/',(req,res)=>{
     res.send("api working")
 })
